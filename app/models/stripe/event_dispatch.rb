c69db5baf747ab05655a28cb56b5bbd1193ce7cd
Maxim Gladkov <contact@maximgladkov.com>
2017-05-04 15:53:48 +0300

    Update event_dispatch.rb

--- app/models/stripe/event_dispatch.rb
+++ app/models/stripe/event_dispatch.rb
@@ -3,13 +3,13 @@ module Stripe
   module EventDispatch
     def dispatch_stripe_event(params)
       retrieve_stripe_event(params) do |evt|
-        target = evt.data.object
+        target = evt.data[:object]
         ::Stripe::Callbacks.run_callbacks(evt, target)
       end
     end
 
     def retrieve_stripe_event(params)
-      id = params['id']
+      id = params[:id]
       if id == 'evt_00000000000000' #this is a webhook test
         yield Stripe::Event.construct_from(params)
       else
